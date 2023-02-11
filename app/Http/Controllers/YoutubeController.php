fe7fbdb28e6a4ffdc9e6facf3047879316ac47d8
Jordan <femi.akoha@epitech.eu>
2023-02-11 15:28:29 +0100

    some push

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -117,15 +117,15 @@ class YoutubeController extends Controller
 
         echo $url;
         $url_components = parse_url($url);
-        parse_str($url_components, $params);
-        echo ' Hi '.$params['name'];
+        
         // $parts = parse_url($url);
         // parse_str($parts['query'], $query);
         // echo $query['email'];
-        // if($_GET['hub_challenge']) {
-        //     $value = $_GET['hub_challenge'];
-        //     return response()->query($value);
-        // }
+        if(isset($_GET['hub_challenge'])) {
+            $value = $_GET['hub_challenge'];
+            // return response()->query($value);
+            echo $value;
+        }
         // else
             // return response()->json('ok');
 
