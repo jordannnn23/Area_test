be04650d46c5bcecc4067c2fa4dfcbdcb9a5528f
Jordan <femi.akoha@epitech.eu>
2023-02-13 12:02:36 +0100

    some push

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -189,7 +189,7 @@ class YoutubeController extends Controller
 
     public function register() {
         // dd(Auth::user()->email);
-        $user_id = Auth::id();
+        // $user_id = Auth::id();
         $hub_url      = "http://pubsubhubbub.appspot.com";
         $callback_url = env('BACKEND_URL')."youtube/callback/".Auth::id();
         dd($callback_url);
