262f53d5e5f30373a9a9ba73c4ef2ecaf01f7897
Jordan <femi.akoha@epitech.eu>
2023-02-13 14:14:18 +0100

    some push

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/auth/google/callback',
+        'redirect' => 'https://secret-fortress-94080.herokuapp.com/auth/google/callback',
     ],
 
 ];
