84f1dc45040dd0bdeaa2d08249f8bed7390b36f2
Jordan <femi.akoha@epitech.eu>
2023-02-21 16:36:39 +0100

    server update

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -163,7 +163,7 @@ class YoutubeController extends Controller
         $message = "You have a new update on your youtube video.\n Go check the video https://youtube.com/channel/".$find_youtube->channel_id;
         $data = [
             'subject' => 'Video Update',
-            'body' => $request->body
+            'body' => $request->query->all()
         ];
         $this->send_mail2($data, $find_user->email);
         if(isset($_GET['hub_challenge'])) {
